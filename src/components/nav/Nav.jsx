0f240361cb89acac49a31e7e3bb0d14bb9f78cd1
TyWalter <walterty89@gmail.com>
2024-06-07 23:45:48 -0500

    Updated the media queries

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -8,22 +8,22 @@ export default function Nav(){
     <nav>
       {/* Homepage/About Me */}
       <NavLink to="/" style={({isActive}) => {
-        return isActive ? {color: "var(--blue)"} : {};
+        return isActive ? {color: "var(--blue)", filter: "drop-shadow(3px 3px 2px var(--black)) drop-shadow(-1px -1px 3px var(--pink))"} : {};
       }} className="navElement">About Me</NavLink>
       
       {/* Portfolio Page */}
       <NavLink to="/portfolio" style={({isActive}) => {
-        return isActive ? {color: "var(--blue)"} : {};
+        return isActive ? {color: "var(--blue)", filter: "drop-shadow(3px 3px 2px var(--black)) drop-shadow(-1px -1px 3px var(--pink))"} : {};
       }} className="navElement">Portfolio</NavLink>
       
       {/* Resumé Page */}
       <NavLink to="/resume" style={({isActive}) => {
-        return isActive ? {color: "var(--blue)"} : {};
+        return isActive ? {color: "var(--blue)", filter: "drop-shadow(3px 3px 2px var(--black)) drop-shadow(-1px -1px 3px var(--pink))"} : {};
       }} className="navElement">Resumé</NavLink>
       
       {/* Contact Page */}
       <NavLink to="/contact" style={({isActive}) => {
-        return isActive ? {color: "var(--blue)"} : {};
+        return isActive ? {color: "var(--blue)", filter: "drop-shadow(3px 3px 2px var(--black)) drop-shadow(-1px -1px 3px var(--pink))"} : {};
       }} className="navElement">Contact Me</NavLink>
     </nav>    
   )
--- src/pages/portfolio/Project.jsx
+++ src/pages/portfolio/Project.jsx
@@ -17,7 +17,7 @@ export default function Project(){
 
   return (
     <>
-      {result};
+      {result}
     </>
   )
 };
